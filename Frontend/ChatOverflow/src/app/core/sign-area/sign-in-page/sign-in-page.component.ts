69cc88275f0faae4e6a5aa5e33fa6105b4c085b4
Marek Schmidt <info@msdev.me>
2019-09-23 23:28:55 +0200

    Implemented sign in api and made submit functional on enter

--- Frontend/ChatOverflow/src/app/core/sign-area/sign-in-page/sign-in-page.component.ts
+++ Frontend/ChatOverflow/src/app/core/sign-area/sign-in-page/sign-in-page.component.ts
@@ -1,5 +1,9 @@
+import { UserSignService } from './../../../services/user/user-sign.service';
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl } from '@angular/forms';
+import { IUserSignIn } from 'src/app/models/api/user-sign-in';
+import { map, catchError } from 'rxjs/operators';
+import { throwError } from 'rxjs';
 
 @Component({
   selector: 'app-sign-in-page',
@@ -13,9 +17,33 @@ export class SignInPageComponent implements OnInit {
     password: new FormControl(''),
   });
 
-  constructor() { }
+  public showLoginFailed = false;
+
+  constructor(
+    private userSignService: UserSignService
+  ) { }
 
   ngOnInit() {
+  }
+
+  login() {
+    const signInData: IUserSignIn = {
+      nameId: this.signInForm.value.nameId,
+      password: this.signInForm.value.password,
+      twoFactorCode: null,
+    };
+    this.userSignService.SignIn(signInData).pipe(
+      map(x => x),
+      catchError(error => {
+        if (error.status === 400) { // Bad request
+          console.error('Login failed...');
+          this.showLoginFailed = true;
+        } else if (error.status === 403) { // Forbidden (locked out)
+          console.log('Forbidden');
+        }
+        return throwError(error);
+      })
+    ).subscribe();
   }
 
 }
