646cd3e67d86ff45e8f589966527897b071a3419
Marek Schmidt <info@msdev.me>
2019-09-23 21:10:35 +0200

    Better random string generator

--- ChatOverflow/ChatOverflow/Utils/RandomString.cs
+++ ChatOverflow/ChatOverflow/Utils/RandomString.cs
@@ -9,23 +9,39 @@ namespace ChatOverflow.Utils
     public class RandomString
     {
 
-        public static string Generate(int length = 50)
+        public static string Generate(int length = 50, string chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890")
         {
-            var randomString = string.Empty;
-            using (var rng = new RNGCryptoServiceProvider())
+            using (RNGCryptoServiceProvider crypto = new RNGCryptoServiceProvider())
             {
-                var bit_count = (length * 6);
-                var byte_count = ((bit_count + 7) / 8); // rounded up
-                var bytes = new byte[byte_count];
-                rng.GetBytes(bytes);
-                randomString = Convert.ToBase64String(bytes);
+                byte[] data = new byte[length];
+                byte[] buffer = null;
+
+                int maxRandom = byte.MaxValue - ((byte.MaxValue + 1) % chars.Length);
+
+                crypto.GetBytes(data);
+                char[] result = new char[length];
+
+                for (int i = 0; i < length; i++)
+                {
+                    byte value = data[i];
+
+                    while (value > maxRandom)
+                    {
+                        if (buffer == null)
+                        {
+                            buffer = new byte[1];
+                        }
+
+                        crypto.GetBytes(buffer);
+                        value = buffer[0];
+                    }
+
+                    result[i] = chars[value % chars.Length];
+                }
+
+                return new string(result);
             }
 
-            if (string.IsNullOrEmpty(randomString))
-                return null;
-
-            return randomString;
         }
-
     }
 }
