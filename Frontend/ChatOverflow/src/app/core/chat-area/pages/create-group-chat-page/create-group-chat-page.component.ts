0de95b02ede82ab2e47ed3d5fc493074f9f02675
Marek Schmidt <info@msdev.me>
2019-09-28 04:39:09 +0200

    And more

--- Frontend/ChatOverflow/src/app/core/chat-area/pages/create-group-chat-page/create-group-chat-page.component.ts
+++ Frontend/ChatOverflow/src/app/core/chat-area/pages/create-group-chat-page/create-group-chat-page.component.ts
@@ -2,7 +2,7 @@ import { UserService } from './../../../../services/user/user.service';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { Observable, of, Subject } from 'rxjs';
-import { debounceTime, map } from 'rxjs/operators';
+import { debounceTime, map, exhaustMap } from 'rxjs/operators';
 import { IUserDetails } from 'src/app/models/user/user-details';
 
 @Component({
@@ -14,6 +14,7 @@ export class CreateGroupChatPageComponent implements OnInit {
 
   public createGroupForm: FormGroup;
   public foundUsers$ = new Subject<IUserDetails[]>();
+  public selectedUsers: IUserDetails[] = [];
 
   private userSearchChanged$ = new Subject<string>();
 
@@ -34,16 +35,18 @@ export class CreateGroupChatPageComponent implements OnInit {
     });
 
     this.userSearchChanged$.pipe(
-      debounceTime(210)
-    ).subscribe(term => {
-      term = term.trim();
-      if (term.length <= 0) { 
-        this.foundUsers$.next([]);
-        return;
-      }
-      this.userService.GetUsersBySearchTerm(term).subscribe(x => {
-        this.foundUsers$.next(x);
-      });
+      debounceTime(150),
+      exhaustMap(term => {
+        term = term.trim();
+        if (term.length <= 0) { 
+          this.foundUsers$.next([]);
+          return of([]);
+        }
+        return this.userService.GetUsersBySearchTerm(term);
+      })
+    ).subscribe(x => {
+      this.selectedUsers.find
+        this.foundUsers$.next(x.filter(y => this.selectedUsers.find(x => x.id === y.id) == null));
     });
 
 
@@ -54,6 +57,16 @@ export class CreateGroupChatPageComponent implements OnInit {
 
   public userSearch(term: string) {
     this.userSearchChanged$.next(term);
+  }
+
+  public selectUser(user: IUserDetails) {
+    if (this.selectedUsers.find(x => x.id === user.id) != null) {
+      return;
+    }
+    this.foundUsers$.next([]);
+    this.selectedUsers = this.selectedUsers.reverse();
+    this.selectedUsers.push(user);
+    this.selectedUsers = this.selectedUsers.reverse();
   }
 
 }
